5145753f079c7ed7e5516d538fe23be142f9e04f
Alan Dipert <alan@dipert.org>
2017-12-01 10:59:20 -0800

    Incorporate ideas from Jenny Bryan

--- day01.R
+++ day01.R
@@ -1,10 +1,13 @@
-day01a <- function(s) {
-  xs <- as.integer(unlist(strsplit(s,"")))
-  sum(ifelse(diff(c(xs, xs[1])) == 0, xs, 0))
+parseInput <- function(s) {
+  as.integer(unlist(strsplit(s, "")))
 }
 
-day01b <- function(s) {
-  xs <- as.integer(unlist(strsplit(s,"")))
-  len <- length(xs)/2
-  sum(ifelse(diff(c(xs, xs[1:len]), lag = len) == 0, xs, 0))
+day01a <- function(s) {
+  xs <- parseInput(s)
+  sum(xs[diff(c(xs, xs[1])) == 0])
+}
+
+day01b <- function(s, lag = length(xs)/2) {
+  xs <- parseInput(s)
+  sum(xs[diff(c(xs, xs[1:lag]), lag = lag) == 0])
 }
