633a32395a41253a75fcc09d90aa931a94443e21
Alan Dipert <alan@dipert.org>
2017-12-09 12:32:33 -0800

    cleanup

--- day08.R
+++ day08.R
@@ -1,33 +1,28 @@
 library(rlang)
 
+get <- function(env, reg) {
+  if (is.null(env[[reg]])) 0 else env[[reg]]
+}
+
 compile <- function(line) {
-  tokens <- unlist(mapply(function(f, x) f(x),
-                   list(identity, identity, as.numeric, sym, identity, identity, as.numeric),
-                   strsplit(line, " ")[[1]]))
-  quo({
-    testVal <- if(is.null(registers[[!!tokens[[5]]]])) 0 else registers[[!!tokens[[5]]]]
-    if (.Primitive(!!tokens[[6]])(testVal, !!tokens[[7]])) {
-      reg <- !!tokens[[1]]
-      prev <- if (is.null(registers[[reg]])) 0 else registers[[reg]]
-      op <- !!if (tokens[[2]] == "inc") "+" else "-"
-      registers[[reg]] <- .Primitive(op)(prev, !!tokens[[3]])
-    }
+  toks <- strsplit(line, " ")[[1]]
+  op_reg <- toks[[1]]
+  op <- list("inc" = "+", "dec" = "-")[[toks[[2]]]]
+  op_arg <- as.numeric(toks[[3]])
+  cmp_reg <- toks[[5]]
+  cmp <- toks[[6]]
+  cmp_arg <- as.numeric(toks[[7]])
+  expr(if (.Primitive(!!cmp)(get(REG, !!cmp_reg), !!cmp_arg)) {
+    newVal <- .Primitive(!!op)(get(REG, !!op_reg), !!op_arg)
+    if (newVal > MAX) MAX <- newVal
+    REG[[!!op_reg]] <- newVal
   })
 }
 
-maxRegister <- function(registers) {
-  if (length(registers) == 0) 0 else max(unlist(as.list(registers)))
-}
-
 run <- function(inputFile) {
-  registers <- new_environment()
-  evalEnv <- new_environment(data = list(registers = registers))
-  maxReg <- 0
-  for (expr in lapply(readLines(inputFile), compile)) {
-    eval_tidy(expr, evalEnv)
-    nextMax <- maxRegister(registers)
-    if (nextMax > maxReg) maxReg <- nextMax
-  }
-  list(highestEnd = maxRegister(registers),
-       highestEver = maxReg)
+  runEnv <- env(REG = new_environment(), MAX = 0)
+  exprs <- lapply(readLines(inputFile), compile)
+  for (e in exprs) eval(e, envir = runEnv)
+  list(maxEnd = max(unlist(as.list(runEnv$REG))),
+       maxAllTime = runEnv$MAX)
 }
